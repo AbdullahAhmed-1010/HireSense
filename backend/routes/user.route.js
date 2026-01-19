b5a39b7fbf027991c2ad9abfe1e1b93c027c6493
Abdullah Ahmed <abdullah.ahmed.pisces.1010@gmail.com>
2026-01-19 18:45:17 +0530

    Setting up multer for file storage

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { login, logout, register, updateProfile } from "../controllers/user.controller.js"
 import authentication from "../middlewares/authentication.js"
+import { singleUpload } from "../middlewares/multer.js"
 
 const router = express.Router()
 
-router.route("/register").post(register)
+router.route("/register").post(singleUpload, register)
 router.route("/login").post(login)
 router.route("/profile/update").post(authentication, updateProfile)
 router.route("/logout").get(logout)
